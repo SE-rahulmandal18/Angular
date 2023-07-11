72b35c3fcd96f73c61ec78f3601bcd5afc6a4b8b
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-11 11:10:26 +0530

    Component LVC

---
+++ my-first-angular-project/src/app/product/product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnChanges, OnDestroy  } from '@angular/core';
+import { Product } from '../classes/Product';
+
+@Component({
+  selector: 'product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit, OnChanges, OnDestroy {
+  // name:string='HP Laptop';
+  // price:number=2000;
+
+  product: Product = new Product("mac",2000,"assets/images/mac.jpg",true,true);
+
+  constructor(){
+    console.log("ProductComponent : constructor called")
+  }
+
+  ngOnInit(){
+    console.log("ProductComponent : ngOnInit called")
+  }
+
+  ngOnChanges(){
+    console.log("ProductComponent : ngOnChanges called")
+  }
+
+  ngOnDestroy(){
+    console.log("ProductComponent : ngOnDestroy called")
+  }
+
+}
