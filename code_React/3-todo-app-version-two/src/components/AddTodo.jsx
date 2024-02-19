38dda5c9305f9ac21e56f234deafe7b89ab58c25
Rahul Mandal <rahulbokaro610@gmail.com>
2024-02-19 10:29:17 +0530

    useRef

--- code_React/3-todo-app-version-two/src/components/AddTodo.jsx
+++ code_React/3-todo-app-version-two/src/components/AddTodo.jsx
@@ -1,53 +1,105 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import { BiMessageAdd } from "react-icons/bi";
+
+// function AddTodo({ onNewItem }) {
+//   const [todoName, setTodoName] = useState("");
+//   const [dueDate, setdueDate] = useState("");
+  
+
+//   const handleNameChange = (event) => {
+//     // console.log(event);
+//     // console.log(event.target.value);
+//     setTodoName(event.target.value);
+//   };
+
+//   const handleDateChange = (event) => {
+//     // console.log(event);
+//     setdueDate(event.target.value);
+//   };
+
+//   const handleAddButtonClicked = (event) => {
+//   //  console.log(event);
+//    event.preventDefault();
+
+//     onNewItem(todoName, dueDate);
+//     setdueDate("");
+//     setTodoName("");
+//   };
+
+//   return (
+//     <div className="container text-center">
+//       <form onSubmit={handleAddButtonClicked} className="row kg-row">
+//         <div className="col-6">
+//           <input
+//             onChange={handleNameChange}
+//             value={todoName}
+//             type="text"
+//             placeholder="Enter Todo Here"
+//           />
+//         </div>
+//         <div className="col-4">
+//           <input type="date" value={dueDate} onChange={handleDateChange} />
+//         </div>
+//         <div className="col-2">
+//           <button
+//             // type="button"
+            
+//             // onClick={handleAddButtonClicked}
+//             className="btn btn-success kg-button"
+//           >
+//             {/* Add */}
+//             <BiMessageAdd />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default AddTodo;
+
+
+
+// enhanced code using ->     useRef 
+
+import { useRef } from "react";
 import { BiMessageAdd } from "react-icons/bi";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState('');
-  const [dueDate, setdueDate] = useState('');
+  const todoNameElememt = useRef();
+  const dueDateElememt = useRef();
 
-  const handleNameChange = (event) => {
-    // console.log(event);
-    // console.log(event.target.value);
-    setTodoName(event.target.value);
+  const handleAddButtonClicked = (event) => {
+    //  console.log(event);
+    event.preventDefault();
+    const todoName = todoNameElememt.current.value;
+    const dueDate = dueDateElememt.current.value;
+    todoNameElememt.current.value = "";
+    dueDateElememt.current.value = "";
+    console.log(`${todoName} due on: ${dueDate}`);
+
+    onNewItem(todoName, dueDate);
   };
-
-  const handleDateChange = (event) => {
-     // console.log(event);
-     setdueDate(event.target.value);
-  };
- 
-
-  const handleAddButtonClicked = () => {
-    onNewItem(todoName, dueDate)
-    setdueDate("");
-    setTodoName("");
-  }
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form onSubmit={handleAddButtonClicked} className="row kg-row">
         <div className="col-6">
           <input
-            onChange={handleNameChange}
-            value={todoName}
             type="text"
+            ref={todoNameElememt}
             placeholder="Enter Todo Here"
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange} />
+          <input type="date" ref={dueDateElememt} />
         </div>
         <div className="col-2">
-          <button
-            onClick={handleAddButtonClicked}
-            type="button"
-            className="btn btn-success kg-button"
-          >
-            {/* Add */}
-            <BiMessageAdd/>
+          <button className="btn btn-success kg-button">
+            <BiMessageAdd />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
