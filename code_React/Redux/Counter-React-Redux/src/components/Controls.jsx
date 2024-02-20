0e9014ff696135f4edeebcff80a2a5d096ba5dbe
Rahul Mandal <rahulbokaro610@gmail.com>
2024-02-20 23:05:31 +0530

    react with redux

--- code_React/Redux/Counter-React-Redux/src/components/Controls.jsx
+++ code_React/Redux/Counter-React-Redux/src/components/Controls.jsx
@@ -13,6 +13,10 @@ const Controls = () => {
     dispatch({ type: "DECREMENT" });
   };
 
+  const handlePrivacyToggle = () => {
+    dispatch({ type: "PRIVACY_TOGGLE" });
+  };
+
   const handleAdd = () => {
     dispatch({
       type: "ADD",
@@ -51,6 +55,7 @@ const Controls = () => {
         >
           -1
         </button>
+        <button type="button" className="btn btn-warning" onClick={handlePrivacyToggle}>Privacy Toggle</button>
       </div>
 
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
