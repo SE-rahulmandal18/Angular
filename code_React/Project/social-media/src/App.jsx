c2699f8118c6994437d7ab420cbb48cf6a8b5025
Rahul Mandal <rahulbokaro610@gmail.com>
2024-02-20 11:28:49 +0530

    updated

--- code_React/Project/social-media/src/App.jsx
+++ code_React/Project/social-media/src/App.jsx
@@ -6,6 +6,7 @@ import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 import { useState } from "react";
+import PostListProvider from "./store/post-list";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
--- code_React/Project/social-media/src/store/post-list.jsx
+++ code_React/Project/social-media/src/store/post-list.jsx
@@ -1,9 +1,54 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({ children });
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
 
-const PostListProvider = () => {
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+//reducer function
+const postListReducer = (currPostList, action) => {
+  return currPostList;
 };
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+
+  const addPost = () => {};
+
+  const deletePost = (postId) => {
+    // console.log(`delete post called for ${postId}`);
+
+    
+
+  };
+
+  return (
+    <PostList.Provider
+      value={{ postList: postList, addPost: addPost, deletePost: deletePost }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Paas ho bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
 
 export default PostListProvider;
