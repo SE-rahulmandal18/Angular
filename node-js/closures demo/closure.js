587a0fb9a30fbeaa49e22a3ed1e7047c06128064
Rahul Mandal <rahulbokaro610@gmail.com>
2024-02-29 10:47:02 +0530

    closures

--- node-js/closures demo/closure.js
+++ node-js/closures demo/closure.js
@@ -1,4 +1,43 @@
 
+/*
+function foo() {
+  // the function scope
+
+  let count = 0;
+  console.log(count);  // logs 0
+}
+
+
+foo();
+
+console.log(count)   // ReferenceError: count is not defined
+
+// outside of the foo() scope, count is inaccessible
+
+*/
+
+
+
+
+
+function outerFunc() {
+  // the outer scope
+  let outerVar = 'I am outside!';
+
+  function innerFunc() {
+    // the inner scope
+
+    console.log(outerVar); // => logs "I am outside!"
+  }
+
+  innerFunc();
+}
+
+outerFunc();
+
+
+
+/*
 function outerFunc() {
   let outerVar = 'I am outside!';
 
@@ -13,3 +52,4 @@ function outerFunc() {
 
 const myInnerFunc = outerFunc();
 myInnerFunc();  // can i still access outervar ?
+*/
