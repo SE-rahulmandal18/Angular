c0d3766dd28871bb430fdad1a6fe10b5c60ccc4f
Rahul Mandal <rahulbokaro610@gmail.com>
2024-02-29 10:41:59 +0530

    closures

---
+++ node-js/closures demo/closure.js
@@ -0,0 +1,15 @@
+
+function outerFunc() {
+  let outerVar = 'I am outside!';
+
+  function innerFunc() {
+
+    console.log(outerVar); // => logs "I am outside!"
+  }
+
+  return innerFunc;
+
+}
+
+const myInnerFunc = outerFunc();
+myInnerFunc();  // can i still access outervar ?
